5d3638c817bb684c00d9e673c0325474987c3a7e
Jace Kelley <jacemk10@gmail.com>
2026-01-27 10:22:35 -0700

    responsive, with javascript and menu transform

---
+++ Week4/ponder/responsive.js
@@ -0,0 +1,32 @@
+// // "use scrict"; 
+// // Means you may not use anything before it is declared
+
+// add(9,8)
+
+// function add(a,b) {
+//     return a + b;
+// }
+
+// // add(a,b) is signature of function. expecting two parameters, and got to call function
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// add(10,33);
+
+// // functions are selfish - they don't care about anyone but themselves
+
+// ans = add(3,6) // 9
+// ans = subtract (ans, 2) // 7
+
+// console.log(ans)
+
+// const subtract = (a,b) => {
+//     return a-b
+// };
+
+// // const subtract = (a,b) => a-b; 
+//     // advanced stuff, shorthand
+
+
